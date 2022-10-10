bbf2caece1d6baae51ae866db50ed3f4d9178632
Mike Elsmore <mike@elsmore.me>
2022-10-10 21:59:33 +0100

    The worst pan and pancake, but it works

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const flipPancake = () => {
-    const pancake = document.getElementById('pancake');
-    pancake.classList.add("doAflip");
-    const pan = document.getElementById('pan');
-    pan.classList.add("doAflip");
-    setTimeout(() => {
-        pancake.classList.remove("doAflip");
-        pan.classList.remove("doAflip");
-    }, 10000)
+const main = async () => {
+    const flipPancake = () => {
+        const kitchen = document.getElementById('kitchen');
+        kitchen.style.display = "block";
+        setTimeout(() => {
+            const pancake = document.getElementById('pancake');
+            pancake.classList.add("doAflip");
+            const pan = document.getElementById('pan');
+            pan.classList.add("doAflip");
+            setTimeout(() => {
+                pancake.classList.remove("doAflip");
+                pan.classList.remove("doAflip");
+                kitchen.style.display = "none";
+            }, 5000)
+        }, 2000)
+    }
+    
+    ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+        if(command === "flip") {
+            flipPancake()
+        }
+    }
+    const url = new URL(window.location.href);
+    ComfyJS.Init(url.searchParams.get('user'));
 }
 
-ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
-    if(command === "flip") {
-        flipPancake()
-    }
-}
-ComfyJS.Init( "ukmadlz" );
+main();
